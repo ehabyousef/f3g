061020f650fabd4bd084749636c82b8143892f0a
‪Ehab Yousef‬‏ <ehabyousef022@gmail.com>
2023-08-31 00:42:43 +0300

    value

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const Contact = lazy(() => import("./Components/Contact"));
 const Notfound = lazy(() => import("./Components/Notfound"));
 const Login = lazy(() => import("./Components/Login"));
 const Signup = lazy(() => import("./Components/Signup"));
-const Team = lazy(() => import("./Components/Team"));c
+const Team = lazy(() => import("./Components/Team"));
 function App() {
   const [navappear, setnavappear] = useState(true);
   let [favItems, setFavItems] = useState([]);
