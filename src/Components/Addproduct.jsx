08d237a893e3175204ee44c124007c77917538f3
‪Ehab Yousef‬‏ <ehabyousef022@gmail.com>
2023-09-11 17:00:52 +0300

    edit

--- src/Components/Addproduct.jsx
+++ src/Components/Addproduct.jsx
@@ -1,4 +1,4 @@
-import React, { useState , useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
@@ -7,43 +7,43 @@ const Addproduct = () => {
     const Navigate = useNavigate()
     const inpref = useRef(null)
 
-    useEffect(()=>{
+    useEffect(() => {
         inpref.current?.focus()
-    },[])
-    
+    }, [])
+
     let handleChange = (e) => {
         console.log(e)
-        const {name,value} = e.target;
-        setProd(old=>({
-            ...old,[name]:value
+        const { name, value } = e.target;
+        setProd(old => ({
+            ...old, [name]: value
         }))
     }
 
-    let addprd=(e)=>{
+    let addprd = (e) => {
         e.preventDefault();
-       axios.post(" http://localhost:2000/products",prod)
-       .then(res=>console.log(res))
-       .catch(err=>console.log(err)) 
-       Navigate('/products')
+        axios.post(" http://localhost:2000/products", prod)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+        Navigate('/products')
     }
     return (
         <div>
             <h1>Add Product</h1>
             <form action="" onSubmit={addprd}>
                 <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="name" name="name" value={prod.name} onChange={handleChange} ref={inpref}/>
+                    <input type="text" className="form-control" id="name" name="name" value={prod.name} onChange={handleChange} ref={inpref} />
                     <label htmlFor="name"> Product Name</label>
                 </div>
                 <div className="form-floating mb-3">
-                    <input type="number" className="form-control" id="floatingInput" name="price" value={prod.price} onChange={handleChange}/>
+                    <input type="number" className="form-control" id="floatingInput" name="price" value={prod.price} onChange={handleChange} />
                     <label htmlFor="floatingInput"> Product Price</label>
                 </div>
                 <div className="form-floating mb-3">
-                    <input type="number" className="form-control" id="floatingInput" name="quantity" value={prod.quantity} onChange={handleChange}/>
+                    <input type="number" className="form-control" id="floatingInput" name="quantity" value={prod.quantity} onChange={handleChange} />
                     <label htmlFor="floatingInput"> Product Quantity</label>
                 </div>
                 <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="floatingInput" name="imgUrl " value={prod.imgUrl} onChange={handleChange}/>
+                    <input type="text" className="form-control" id="floatingInput" name="imgUrl " value={prod.imgUrl} onChange={handleChange} />
                     <label htmlFor="floatingInput"> Product Image</label>
                 </div>
                 <button className="btn btn-danger">Submit</button>
@@ -51,5 +51,4 @@ const Addproduct = () => {
         </div>
     );
 };
-
 export default Addproduct;
